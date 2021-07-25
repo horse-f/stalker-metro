4fb8f48ae41eec8f8a2470f7a9d913480251ce89
Timothy Meister <tmeister1212@gmail.com>
2021-07-25 14:51:53 -0500

    CC-AimDownSights: ADSWIP

--- Assets/character/characterController.cs
+++ Assets/character/characterController.cs
@@ -6,17 +6,27 @@ using UnityEngine;
 public class characterController : MonoBehaviour {
     public float accel = 30.0f;
     public float sprintAccel = 60.0f;
-    public float cameraZoom = 15.0f;
+    public float cameraSize = 15.0f;
+    public Transform aimPoint = null;
+    public float zoomPadding = 2.0f;
+    public float zoomSize = 13.0f;
     
     private Rigidbody2D rigidbody2d;
     private bool sprinting = false;
+    private bool aiming = false;
+    private bool aimingOnce = false;
+    private Camera mainCamera;
+    private float zoomRadius = 0.0f;
 
     private void Start() {
+        mainCamera = Camera.main;
+        mainCamera.orthographicSize = cameraSize;
         rigidbody2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
     private void Update() {
-        input();
+        GetInput();
+        GetAim();
     }
 
     private void FixedUpdate() {
@@ -27,16 +37,38 @@ public class characterController : MonoBehaviour {
         }
     }
 
-    private void input() {
+    private void GetInput() {
+        // aiming = Input.GetButtonDown("ADS") ? !aiming : aiming;
+        aiming = Input.GetButton("ADS");
+        Debug.Log("Aiming: " + aiming);
+        sprinting = Input.GetButtonDown("Sprint") ? !sprinting : sprinting;
         if(Input.GetButtonDown("Inventory")) {
-            // implement this
             Debug.Log("Inventory");
         } 
         if(Input.GetButtonDown("Map")) {
-            // implement this
             Debug.Log("Map");
         }
-        sprinting = Input.GetButtonDown("Sprint") ? !sprinting : sprinting;
+        if(Input.GetButtonDown("Fire")) {
+            Debug.Log("Pew");
+        }
+        if(aiming) {
+            Debug.Log("AimDownSights");
+            aimingOnce = true;
+            AimDownSights();
+        } else if (!aiming && aimingOnce) {
+            Debug.Log("ResetAim");
+            aimingOnce = false;
+            ResetAim();
+        }
+    }
+
+    private void GetAim() {
+        Vector3 mousePoint = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        mousePoint.z = 0;
+        if(aimPoint) {
+            aimPoint.transform.LookAt(mousePoint + Vector3.right * aimPoint.transform.position.z);
+            Debug.DrawRay(aimPoint.position, aimPoint.forward * 6, Color.yellow);
+        }
     }
 
     private void Move(Vector2 input) {
@@ -49,5 +81,24 @@ public class characterController : MonoBehaviour {
             }
         }
         rigidbody2d.AddForce((transform.up + transform.right) * input.normalized * a);
+    }
+
+    private void AimDownSights() {
+        Vector2 zoomPoint = GetZoomPoint();
+        // find zoom point
+            // get mouse x,y
+            // get point in direction of mouse x,y at zoomRadius
+        mainCamera.transform.position = new Vector3(zoomPoint.x, zoomPoint.y, -10);
+        // lerp camera to zoom point
+        // change camera size to zoomSize
+    }
+
+    private Vector2 GetZoomPoint() {
+        return new Vector2(0,0);
+    }
+
+    private void ResetAim() {
+        // lerp camera back to default position
+        mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
     }
 }
